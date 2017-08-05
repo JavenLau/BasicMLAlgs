f3263f040d79f9c337d9fa2c3369a859e79a4023
JavenLau <javenlau@hotmail.com>
2017-08-05 19:12:58 +0800

    add bayes

--- knn_matlab/knn.m
+++ knn_matlab/knn.m
@@ -12,22 +12,23 @@ load('testLabel.mat');
 k=30;
 numA = 2;
 numB = 8;
-num = 1000;
+num = 100;
 
 % reconstruct the data
 [newTrainLabel, newTrainData] = consData(trainLabel, trainData, num, numA, numB);
 [newTestLabel, newTestData] = consData(testLabel, testData, num, numA, numB);
 
+tstart = tic;
 % 
 % compute the distance & learning and classify
-distance = zeros(200,1);
+distance = zeros(num*2,1);
 kNearst = zeros(k,1);
 kNearstValue = zeros(k,1);
 kNearstLabel = zeros(k,1);
 error = 0;
-for i = 1:200
-    
-    for ii = 1:200  %compute the distance
+for i = 1:num*2
+%     i
+    for ii = 1:num*2  %compute the distance
         diff = abs( newTestData(i,:)-newTrainData(ii,:) );
         distance(ii) = sum(sum(diff));
     end;
@@ -41,9 +42,9 @@ for i = 1:200
         error = error+1;
     end;
 end;
-
 errorRate = error*100/(num*2)
 
+time = toc(tstart)
 
 
 
---
+++ naiveBayes_matlab/naiveBayes.m
@@ -0,0 +1,102 @@
+
+clc
+clear all;
+close all;
+
+addpath('.././data/mnist');%read the upper path
+
+load('trainData.mat');
+load('trainLabel.mat');
+load('testData.mat');
+load('testLabel.mat');
+
+% % 
+% % data pre-pocessing
+[num, unused] = size(testLabel);
+for i= 1:num
+    [unused, label] = max( testLabel(i,:) );
+    ntestLabel(i) = label;
+end;
+
+[num, unused] = size(trainLabel);
+for i= 1:num
+    [unused, label] = max( trainLabel(i,:) );
+    ntrainLabel(i) = label;
+end;
+
+trainData(trainData~=0)=1;%binaryzation
+testData(testData~=0)=1;
+
+% % 
+% % compute the prior probability & conditional probability
+for label=1:10
+    disp([label]);
+    %take out all the data with the given label
+    tempData=[];    %matrix init
+    rows=0;
+    for i=1:60000
+%         disp([label, i]);
+        if(ntrainLabel(i) == label)
+            rows=rows+1;
+            tempData(rows,:)=trainData(i,:);
+        end;
+    end;
+%     size(tempData)
+    
+    labelNum = sum(ntrainLabel == label);
+    PP(label) = (labelNum+1)/(num+10);   %% prior probability
+    for fd=1:784 %feature dimension        
+        for fdv=0:1         %feature value
+            temp = sum( tempData(:,fd)==fdv )+1;
+%             if(temp > labelNum+2)
+%                 disp([temp, labelNum+2]);
+%             end;
+            CP(fd,fdv+1,label) = temp / (labelNum+2);     %% conditional probability           
+        end;
+    end;   
+end;
+
+% % 
+% % compute the posterior probability
+error = 0;
+for num=1:10000
+%     disp([num])
+    POP=ones(1,10);
+    for label=1:10        
+        for fd=1:784 %feature dimension
+            temp = CP(fd,testData(num,fd)+1,label)/10000+1.0002; %set the conditional probability between 1.0001 and 1.0002
+%             if(temp>1.0002 || temp<1.0001)
+%                 disp([temp]);
+%             end;
+            POP(label)= POP(label)*temp;    
+        end;
+%         POP(label)= temp;
+%         POP(label)= PP(label)*POP(label);
+    end;
+    [unused, cLabel] = max(POP);
+    if(cLabel ~= ntestLabel(num))
+        error = error+1;
+    end;   
+end;
+
+accuracy = 100-error*100/10000
+
+
+% % 
+% % show the sample image
+% trainImage = zeros(28,28);
+% for i=100:100
+%     for j=1:784
+%         jj = rem(j,28);
+%         if(rem(j,28) == 0)
+%             jj=28;
+%         end;
+%         trainImage(ceil(j/28), jj) = trainData(i,j);      
+%     end;
+% %     imshow(trainImage, []);   
+% %     pause;
+% %     fprintf('\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b');  %delete the previous content 
+% end;
+% imwrite(trainImage,'trainImage.jpg');
+% 
+% [trainImage, descriptors, locs] = sift('trainImage.jpg');
