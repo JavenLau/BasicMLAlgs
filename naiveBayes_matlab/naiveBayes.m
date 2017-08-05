647c21af76b2b560f3e6e3934446c384cf73573a
JavenLau <javenlau@hotmail.com>
2017-08-05 19:15:27 +0800

    add bayes

--- naiveBayes_matlab/naiveBayes.m
+++ naiveBayes_matlab/naiveBayes.m
@@ -80,23 +80,3 @@ for num=1:10000
 end;
 
 accuracy = 100-error*100/10000
-
-
-% % 
-% % show the sample image
-% trainImage = zeros(28,28);
-% for i=100:100
-%     for j=1:784
-%         jj = rem(j,28);
-%         if(rem(j,28) == 0)
-%             jj=28;
-%         end;
-%         trainImage(ceil(j/28), jj) = trainData(i,j);      
-%     end;
-% %     imshow(trainImage, []);   
-% %     pause;
-% %     fprintf('\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b');  %delete the previous content 
-% end;
-% imwrite(trainImage,'trainImage.jpg');
-% 
-% [trainImage, descriptors, locs] = sift('trainImage.jpg');
