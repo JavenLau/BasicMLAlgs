48466d653b5341475dc8fc726622e487a5d7585e
JavenLau <javenlau@hotmail.com>
2017-08-20 02:25:12 +0800

    tree modified

--- decisionTree_matlab/featureSelection.m
+++ decisionTree_matlab/featureSelection.m
@@ -4,6 +4,10 @@ function[ig] = featureSelection(data, label)
 [unused, f] = size(data); %the numbers of features
 % f
 for i = 1:f
+    if(max(data(:,i))==3 || isenpty(data))
+        ig(i) = 0;
+        continue;
+    end;
     ig(i)=computeInfoGain(data, label, i);
 end;
 % fprintf('info gain %s\n', num2str(ig));
